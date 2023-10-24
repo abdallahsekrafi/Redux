0374d1384c2b876393f242e26a993e2791f655ed
Sekrafi Abdallah <abdallah.sek@gmail.com>
2023-10-24 11:51:25 +0100

    Update Task.js

--- src/component/Task.js
+++ src/component/Task.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "../App.css";
 import { Stack } from "@mui/material";
+import Divider from "@mui/material/Divider";
 import DoneIcon from "@mui/icons-material/Done";
 import EditModel from "../actions/editModel";
+import Badge from "react-bootstrap/Badge";
 
 const Task = ({ todo }) => {
   return (
@@ -11,7 +13,7 @@ const Task = ({ todo }) => {
         direction="row"
         justifyContent="space-between"
         alignItems="center"
-        spacing={0}
+        spacing={1}
       >
         <Stack
           direction="row"
@@ -27,7 +29,30 @@ const Task = ({ todo }) => {
           <p>{todo.description}</p>
         </Stack>
 
-        <EditModel todoValue={todo} />
+        <Stack
+          direction="row"
+          justifyContent="flex-end"
+          alignItems="center"
+          spacing={1}
+          divider={
+            <Divider
+              style={{ backgroundColor: "rgb(162, 167, 162)" }}
+              orientation="vertical"
+              flexItem
+            />
+          }
+        >
+          {todo.isdone ? (
+            <h6>
+              <Badge bg="success">Done</Badge>
+            </h6>
+          ) : (
+            <h6>
+              <Badge bg="secondary">Not yet</Badge>
+            </h6>
+          )}
+          <EditModel todoValue={todo} />
+        </Stack>
       </Stack>
     </div>
   );
